ba05c15a16c06c0f93dbc2e115985223e1efe1c6
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-12-09 22:32:21 +0100

    Fix extension name

--- wordslab_notebooks_lib/__init__.py
+++ wordslab_notebooks_lib/__init__.py
@@ -1,8 +1,8 @@
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __all__ = []
 
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "wordslab-notebooks-extension"
+        "dest": "wordslab-notebooks-lib"
     }]
